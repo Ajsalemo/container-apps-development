7187486305cf268723b5a0f7b21e1bd3225603f0
Anthony Salemo <ajssalemo@gmail.com>
2022-04-12 19:46:52 -0400

    Able to POST to a storage component in Dapr

--- node/state-management-http/controllers/getStateController.js
+++ node/state-management-http/controllers/getStateController.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const axios = require("axios").default;
 const router = express.Router();
 
 const getStateController = router.get("/", async (_, res, next) => {
   try {
     const getOrderArr = [];
     for (i = 0; i < 10; i++) {
-      const { res } = await axios.post(`${DAPR_HOST}:${DAPR_HTTP_PORT}/v1.0/state/${DAPR_STATE_STORE}/${i}`);
+      const { res } = await axios.get(`${DAPR_HOST}:${DAPR_HTTP_PORT}/v1.0/state/${DAPR_STATE_STORE}/${i}`);
       console.log(res)
+      getOrderArr.push(res)
     }
     res.json({ "message": postOrderArr })
   } catch (error) {
--- node/state-management-http/controllers/postStateController.js
+++ node/state-management-http/controllers/postStateController.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
-const { BASE_URL } = require("../config/config")
+const axios = require("axios").default;
+const { BASE_URL } = require("../config/config");
 
 const postStateController = router.get("/", async (_, res, next) => {
-  console.log(BASE_URL)
+  console.log(BASE_URL);
   try {
-    const postOrderArr = [];
-    for (i = 0; i < 10; i++) {
-      const state = [
-        {
-          key: i,
-          value: `takeout order ${i}`,
-        },
-      ];
-      postOrderArr.push(state);
-      const { res } = await axios.post(`${BASE_URL}`, state);
-      console.log(res)
-    }
-    res.json({ "message": postOrderArr })
+    // const postOrderArr = [];
+    // for (i = 1; i < 10; i++) {
+    //   const state = {
+    //     key: i,
+    //     value: `takeout order ${i}`,
+    //   };
+
+    //   postOrderArr.push(state);
+    // }
+    const state = [{
+      key: "1",
+      value: "takeout order: 1",
+    }]
+
+    const { response: data } = await axios.post(`${BASE_URL}`, state);
+    console.log(data);
+    res.json({ message: data });
   } catch (error) {
     console.log("An error has occurred: ", error);
     next(error);
