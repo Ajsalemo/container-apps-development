e6e9218e72636607bcbf9610179ae9c895e00f75
Anthony Salemo <ajssalemo@gmail.com>
2022-05-04 14:02:24 -0400

    updated some variable naming in the msi example

--- python/managed-identity/app.py
+++ python/managed-identity/app.py
@@ -6,17 +6,17 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    keyVaultName = os.environ["KEY_VAULT_NAME"]
-    secretName = os.environ["SECRET_NAME"]
-    KVUri = f"https://{keyVaultName}.vault.azure.net"
+def get_keyvault_secret():
+    key_vault_name = os.environ["KEY_VAULT_NAME"]
+    secret_name = os.environ["SECRET_NAME"]
+    kv_uri = f"https://{key_vault_name}.vault.azure.net"
 
     credential = DefaultAzureCredential()
-    client = SecretClient(vault_url=KVUri, credential=credential)
+    client = SecretClient(vault_url=kv_uri, credential=credential)
 
-    print(f"Retrieving your secret from {keyVaultName}.")
+    print(f"Retrieving your secret from {key_vault_name}.")
 
-    retrieved_secret = client.get_secret(secretName)
+    retrieved_secret = client.get_secret(secret_name)
 
     print(f"Your secret is '{retrieved_secret.value}'.")
     print("Done.")
