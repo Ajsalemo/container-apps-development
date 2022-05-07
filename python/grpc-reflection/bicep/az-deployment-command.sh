9fef2ab5bdae9e19a664484f63439248e5fa92d7
Anthony Salemo <ajssalemo@gmail.com>
2022-05-06 23:35:07 -0400

    adding a grpc example with server reflection

---
+++ python/grpc-reflection/bicep/az-deployment-command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+az deployment group create \
+  --resource-group "$RESOURCE_GROUP" \
+  --template-file ./grpc-reflection.bicep \
+  --parameters \
+    environment_name="$CONTAINERAPPS_ENVIRONMENT" \
+    location="$LOCATION" \
+    azureContainerRegistry="$AZURE_CONTAINER_REGISTRY" \
+    azureContainerRegistryUsername="$AZURE_CONTAINER_REGISTRY_USERNAME" \
+    azureContainerRegistryPassword="$AZURE_CONTAINER_REGISTRY_PASSWORD"
